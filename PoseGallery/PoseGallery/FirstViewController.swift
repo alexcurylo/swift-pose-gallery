4cd2d7cde0036438bc989007acc9be3ba956bff9
Alex Curylo <alex@alexcurylo.com>
2014-12-06 12:17:15 +0530

    Default Xcode 6.1 Swift/Universal Tabbed Application shell.

---
+++ PoseGallery/PoseGallery/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PoseGallery
+//
+//  Created by alex on 2014-11-27.
+//  Copyright (c) 2014 Trollwerks Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PoseGallery/PoseGallery/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PoseGallery
+//
+//  Created by alex on 2014-11-27.
+//  Copyright (c) 2014 Trollwerks Inc. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
