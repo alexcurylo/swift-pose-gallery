f6a44fb46cfd3f77d60f8399a583c50c9ef504f6
Alex Curylo <alex@alexcurylo.com>
2014-12-21 15:31:17 -0800

    Working install of Fabric
    Install goes much closer to as intended when you're in North America, apparently.

--- PoseGallery/PoseGalleryConfig/script_build_end.sh
+++ PoseGallery/PoseGalleryConfig/script_build_end.sh
@@ -8,7 +8,7 @@
 
 # Crashlytics magic 
 
-./Fabric.framework/run 186ef2a41f30e2ce39a21f35b61600d3ae927290 3ce3168d4276f7278273f34fbc45d96dd492c71a98dc7a3dcd8f1fc3da321e50
+./PoseGallery/libraries/Fabric.framework/run 186ef2a41f30e2ce39a21f35b61600d3ae927290 3ce3168d4276f7278273f34fbc45d96dd492c71a98dc7a3dcd8f1fc3da321e50
 
 # reveal the binary in the Finder if you like
 
