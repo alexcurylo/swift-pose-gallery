8732b31cb5c48a066abc26216083e0a6793ffd92
Alex Curylo <alex@alexcurylo.com>
2015-04-25 23:06:55 -0700

    Build reporting
    Warnings for TODO and FIXME; enable Core Data thread watchdog

--- PoseGallery/PoseGalleryConfig/script_build_begin.sh
+++ PoseGallery/PoseGalleryConfig/script_build_begin.sh
@@ -13,6 +13,12 @@ if [ $CONFIGURATION != "Release" ] ; then
 ${SRCROOT}/PoseGalleryConfig/script_tag_icon.sh tag PoseGallery/resources/Images.xcassets/AppIcon.appiconset
 fi
 
+# undone work warnings
+
+TAGS="TODO:|FIXME:"
+echo "searching ${SRCROOT} for ${TAGS}"
+find "${SRCROOT}" \( -name "*.swift" \) -print0 | xargs -0 egrep --with-filename --line-number --only-matching "($TAGS).*\$" | perl -p -e "s/($TAGS)/ warning: \$1/"
+
 # finished
 
 return 0
